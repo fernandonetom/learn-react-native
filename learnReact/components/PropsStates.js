cc703b17e9b32c5ee2afbe4a4ca72272f13978cb
fernando39357 <fernando39357@gmail.com>
2020-07-13 10:35:15 -0300

    setState by input

--- learnReact/components/PropsStates.js
+++ learnReact/components/PropsStates.js
@@ -1,20 +1,36 @@
 import React, {useState} from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import styled from 'styled-components/native';
 
 const Page = styled.SafeAreaView`
   flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const Input = styled.TextInput`
+  padding: 10px;
+  height: 60px;
+  width: 300px;
+  border: 1px solid #ccc;
 `;
 
 const Hello = (props) => {
   const [name, setName] = useState('Teste');
-  return <Text>Nome: {name}</Text>;
+  function mudarTexto(texto) {
+    setName(texto);
+  }
+  return (
+    <View>
+      <Input value={name} onChangeText={mudarTexto} />
+      <Text>Olá {name}</Text>
+    </View>
+  );
 };
 
 export default () => {
   return (
     <Page>
-      <Hello>Meu teste</Hello>
+      <Hello />
     </Page>
   );
 };
