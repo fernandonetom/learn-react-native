b298d3b8965725f451aded1ac6e3e9fa02c1afef
fernando39357 <fernando39357@gmail.com>
2020-07-17 15:36:55 -0300

    Redux Persist

--- learnReact/App.js
+++ learnReact/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import {PersistGate} from 'redux-persist/es/integration/react';
 import {Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import MainStack from './components/Navigator/Stack';
 import MainTab from './components/Navigator/Tab';
 import {Provider} from 'react-redux';
-import Store from './components/Store';
+import {store, persistor} from './components/Store';
 
 export default () => {
   return (
-    <Provider store={Store}>
-      <NavigationContainer>
-        <MainTab />
-      </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <MainTab />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 };
--- learnReact/components/Store.js
+++ learnReact/components/Store.js
@@ -1,4 +1,20 @@
 import {createStore} from 'redux';
+import AsyncStorage from '@react-native-community/async-storage';
+import {persistStore, persistReducer} from 'redux-persist';
+
 import Reducers from './Reducers';
-const store = createStore(Reducers);
-export default store;
+
+const persistedReducer = persistReducer(
+  {
+    key: 'root',
+    storage: AsyncStorage,
+    whitelist: ['userReducer'],
+    //blacklist:[]
+  },
+  Reducers,
+);
+
+const store = createStore(persistedReducer);
+
+const persistor = persistStore(store);
+export {store, persistor};
