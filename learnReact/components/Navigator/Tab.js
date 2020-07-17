1ad9c69d772f78c7799e3bb7131af7e344c993c9
fernando39357 <fernando39357@gmail.com>
2020-07-17 10:52:19 -0300

    Tab icons

--- learnReact/components/Navigator/Tab.js
+++ learnReact/components/Navigator/Tab.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import {Image} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import TabHomeScreen from '../pages/TabHome';
 import TabAboutScreen from '../pages/TabAbout';
-
+import Icon from 'react-native-vector-icons/Feather';
+Icon.loadFont();
 const Tab = createBottomTabNavigator();
 
 export default () => (
-  <Tab.Navigator>
-    <Tab.Screen name="TabHome" component={TabHomeScreen} />
-    <Tab.Screen name="TabAbout" component={TabAboutScreen} />
+  <Tab.Navigator
+    screenOptions={{
+      tabBarLabel: 'Valor padrão',
+    }}>
+    <Tab.Screen
+      name="TabHome"
+      component={TabHomeScreen}
+      options={{
+        tabBarLabel: 'Página inicial',
+        tabBarIcon: ({focused}) => {
+          return (
+            <Icon name="home" size={20} color={focused ? '#FF0000' : '#333'} />
+          );
+        },
+      }}
+    />
+    <Tab.Screen
+      name="TabAbout"
+      component={TabAboutScreen}
+      options={{
+        tabBarLabel: 'Sobre nós',
+        tabBarIcon: ({focused}) => {
+          return (
+            <Icon name="info" size={20} color={focused ? '#FF0000' : '#333'} />
+          );
+        },
+      }}
+    />
   </Tab.Navigator>
 );
